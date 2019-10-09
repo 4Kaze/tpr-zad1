bab404c2c249a487383c5637628f7707401dfbd0
TinkerGlass <56132402+TinkerGlass@users.noreply.github.com>
2019-10-09 17:37:24 +0200

    Add files via upload

---
+++ Zad1/Catalog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class Catalog
+    {
+        public Catalog( string NewCode, string Name)
+        {
+            Code = NewCode;
+            ProductName = Name;
+            Details = "";
+        }
+        public string Code { get; }
+        public string Details { get; set; }
+        public string ProductName { get; }
+    }
+}
---
+++ Zad1/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class Client
+    {
+        
+        public string Code { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public string Adress { set; get; }
+
+
+
+        public Client(string Code, string Name, string Surname, string Adress)
+        {
+            this.Code = Code;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Adress = Adress;
+        }
+
+
+        public Client(string Code, string Name, string Surname)
+        {
+            this.Code = Code;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Adress = "";
+        }
+
+    }
+}
