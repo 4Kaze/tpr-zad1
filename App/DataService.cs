9994610328c9514ad170c3208e38361eab776180
Bartek <btmikolajczyk@wp.pl>
2019-10-14 15:57:31 +0200

    zrobilem pol czwartego ale nie weim jak do konca stestowac

--- App/DataService.cs
+++ App/DataService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Zad1;
 
 namespace App
@@ -10,5 +12,57 @@ namespace App
         {
             this.repositoryInterface = repositoryInterface;
         }
+
+
+
+        public string ViewList(IEnumerable<Object> items)
+        {
+            string answer = "";
+            foreach (Object exampleObject in items)
+            {
+                answer += exampleObject.ToString() + "\n";
+            }
+            return answer;
+        }
+
+
+
+
+
+
+        public string ViewList(Dictionary<long, Catalog> items)
+        {
+            string answer = "";
+            foreach (Object exampleObject in items)
+            {
+                answer += exampleObject.ToString() + "\n";
+            }
+            return answer;
+        }
+
+
+        //zaczynając od elementów wykazu(np.czytelnicy, klienci),
+        //za nimi zdarzenia odpowiadające kolejnym elementom wykazu(np.wypożyczenia książek, faktury),
+        //które przechodząc przez opisy stanu będą indentyfikować pozycje katalogu(np.wypożyczone książki, zakupione towary).
+
+
+        public string FullView()
+        {
+            string answer = "";
+            foreach (Person person in repositoryInterface.GetAllPersons())
+            {
+                answer += person.ToString() + "\n";
+                foreach(Event happening in repositoryInterface.GetAllTransactions())
+                {
+                    if(happening.Causer == person)
+                    {
+                        answer += happening.BookState.Book.ToString() + "\n";
+                    }
+                }
+            }
+            return answer;
+        }
+
+
     }
 }
---
+++ UnitTests/DataServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Zad1;
+using App;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DataServiceTests
+    {
+        public object MessageBox { get; private set; }
+
+        [TestMethod]
+        public void DataServiceConstructorTest()
+        {
+            DataService dataService = new DataService(new DataRepository(new FillConstant()));
+        }
+
+
+        [TestMethod]
+        public void DataServiceGenerateViewOfList()
+        {
+            // nie wiem jak to stestowac
+            DataService dataService = new DataService(new DataRepository(new FillConstant()));
+            IFillInterface fillInterface = new FillRandom();
+            DataContext dataContext = new DataContext();
+            fillInterface.FillData(dataContext);
+            string a = dataService.ViewList(dataContext.Transactions);
+            string b = "";
+        }
+
+
+        [TestMethod]
+        public void DataServiceAllView()
+        {
+            // nie wiem jak to stestowac
+            DataService dataService = new DataService(new DataRepository(new FillConstant()));
+            IFillInterface fillInterface = new FillRandom();
+            DataContext dataContext = new DataContext();
+            fillInterface.FillData(dataContext);
+            string a = dataService.FullView();
+            string b = "";
+        }
+
+    }
+}
