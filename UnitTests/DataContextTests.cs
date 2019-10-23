ec3c12f2d31378a29c35f4c40b2f2227c55134e3
Bartek <btmikolajczyk@wp.pl>
2019-10-23 21:07:51 +0200

    deleted XML file reading

--- UnitTests/DataContextTests.cs
+++ UnitTests/DataContextTests.cs
@@ -44,18 +44,5 @@ namespace UnitTests
             Assert.AreEqual(amount, dataContext.Descriptions.Count);
             Assert.AreEqual(amount, dataContext.Transactions.Count);
         }
-
-
-        [TestMethod]
-        public void EqualsEventMethodTest()
-        {
-            IFillInterface fillInterface = new FillFileData();
-            DataContext dataContext = new DataContext();
-            fillInterface.FillData(dataContext);
-            Assert.AreEqual(3, dataContext.Books.Count);
-            Assert.AreEqual(3, dataContext.Clients.Count);
-            Assert.AreEqual(3, dataContext.Descriptions.Count);
-            Assert.AreEqual(3, dataContext.Transactions.Count);
-        }
     }
 }
