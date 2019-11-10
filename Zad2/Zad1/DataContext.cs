bb666a3109ef63e3fcf33b1641c35b52fcf1c9d2
4kaze <32jacke32@gmail.com>
2019-11-10 13:34:20 +0100

    XML dictionary deserialization

--- Zad2/Zad1/DataContext.cs
+++ Zad2/Zad1/DataContext.cs
@@ -11,49 +11,22 @@ namespace Classes
     {
         [XmlArray("Clients")]
         public List<Person> Clients;
-        [XmlIgnore]
-        public Dictionary<long, Catalog> Books;
+        [XmlElement("Books")]
+        public SerializableCatalogDictionary Books;
         [XmlArray("Transactions")]
         public ObservableCollection<Event> Transactions;
         [XmlArray("Descriptions")]
         public List<StateDescription> Descriptions;
 
-        [XmlArray("Books")]
-        [XmlArrayItem("Book")]
-        public List<KeyValuePair<long, Catalog>> BooksList
-        {
-            get
-            {
-                return Books.Select(pair => new KeyValuePair<long, Catalog>(pair.Key, pair.Value)).ToList();
-            }
-            set
-            {
-                Books = value.ToDictionary(x => x.Key, x => x.Value);
-            }
-        }
-
         public DataContext()
         {
             Clients = new List<Person>();
-            Books = new Dictionary<long, Catalog>();
+            Books = new SerializableCatalogDictionary();
             Transactions = new ObservableCollection<Event>();
             Descriptions = new List<StateDescription>();
         }
 
-        [Serializable]
-        public struct KeyValuePair<K, V>
-        {
-            public KeyValuePair(K k, V v) {
-                Key = k;
-                Value =v;
-            }
-                
-            public K Key
-            { get; set; }
-
-            public V Value
-            { get; set; }
-        }
+        
 
 
     }
---
+++ Zad2/Zad1/SerializableCatalogDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Classes
+{
+    public class SerializableCatalogDictionary : Dictionary<long, Catalog>, IXmlSerializable
+    {
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<KeyValuePair>));
+            reader.Read();
+            foreach(KeyValuePair pair in (List<KeyValuePair>)serializer.Deserialize(reader))
+            {
+                Add(pair.Key, pair.Value);
+            }
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            
+            XmlSerializer serializer = new XmlSerializer(typeof(List<KeyValuePair>));
+            serializer.Serialize(writer, this.Select(pair => new KeyValuePair(pair.Key, pair.Value)).ToList());
+        }
+
+        [Serializable]
+        [XmlType("CatalogKeyValuePair")]
+        public struct KeyValuePair
+        {
+            public KeyValuePair(long k, Catalog v)
+            {
+                Key = k;
+                Value = v;
+            }
+
+            public long Key
+            { get; set; }
+
+            public Catalog Value
+            { get; set; }
+        }
+    }
+}
